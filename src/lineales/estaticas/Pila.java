b90d01355e448bbe36db0fba5694281363deabde
Luis Keller <luis.keller@est.fi.uncoma.edu.ar>
2023-04-06 18:31:52 -0300

    Finalización Implementación Pila Estática
    Se implementan todos los métodos y constructores de Pila Estática

--- src/lineales/estaticas/Pila.java
+++ src/lineales/estaticas/Pila.java
@@ -10,38 +10,103 @@ KELLER LUIS EULOGIO, Legajo FAI-2321
 */
 public class Pila implements PilaInterface{
 
+    private static int TAM = 10;
+    private Object[] arreglo;
+    private int tope;
+    
+    public Pila(){
+        this.arreglo = new Object[TAM];
+        this.tope = -1;
+    }
+    
     @Override
     public boolean apilar(Object elemento) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean resultado = false;
+        if(this.tope != TAM-1){
+            this.arreglo[this.tope+1] = elemento;
+            this.tope += 1;
+            resultado = true;
+        }
+        return resultado;
     }
 
     @Override
     public boolean desapilar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean resultado = false;
+        if(this.tope != -1)
+        {
+            this.arreglo[this.tope] = null;
+            this.tope -= 1;
+            resultado = true;
+        }
+        return resultado;
     }
 
     @Override
     public Object obtenerTope() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return this.arreglo[this.tope];
     }
 
     @Override
     public boolean esVacia() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean resultado = false;
+        if(this.tope == -1)
+        {
+            resultado = true;
+        }
+        return resultado;
     }
 
     @Override
     public void vaciar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //Verifica que la Pila no esté vacía
+        if(this.tope != -1)
+        {
+            while(this.tope >= 0)
+            {
+                this.arreglo[this.tope] = null;
+                this.tope -= 1;
+            }
+        }
     }
 
     @Override
     public Pila clone() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //Creo nueva Pila
+        Pila pilaClonada = new Pila();
+        //Recorro los elementos de la Pila a clonar, si es que no está vacía.
+        if(this.tope != -1)
+        {
+            int aux = 0;
+            while(aux <= this.tope)
+            {
+                pilaClonada.apilar(this.arreglo[aux]);
+                aux += 1;
+            }
+        }
+        return pilaClonada;
     }
     
     @Override
     public String toString(){
-        return "";
+        String resultado = "";
+        if(this.tope == -1)
+        {
+            resultado = "Pila vacía";
+        }else{
+            resultado = "[";
+            //Recorro toda la Pila, desde el tope hasta la base
+            int aux = this.tope;
+            while(aux >= 0)
+            {
+                resultado += this.arreglo[aux].toString();
+                aux -= 1;
+                if(aux > 0){
+                    resultado += ",";
+                }
+            }
+            resultado += "]";
+        }
+        return resultado;
     }
 }
