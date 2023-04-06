fb7a2a72d94dc9ca231b6e9cee01c4007303b3cc
Luis Keller <luis.keller@est.fi.uncoma.edu.ar>
2023-04-06 18:13:13 -0300

    Finalización Pila dinámica
    Se implementa completamente la clase Pila dinámica

--- src/lineales/dinamicas/Pila.java
+++ src/lineales/dinamicas/Pila.java
@@ -12,39 +12,107 @@ KELLER LUIS EULOGIO, Legajo FAI-2321
 */
 public class Pila implements PilaInterface{
 
+    private Nodo tope;
+    
+    //Constructor vacío
+    public Pila(){
+        this.tope = null;
+    }
+    
     @Override
     public boolean apilar(Object elemento) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //Crea un nuevo nodo, con el elemento ingresado y enlace igual a tope
+        Nodo nuevo = new Nodo(elemento, this.tope);
+        //El nodo nuevo es el nuevo tope
+        this.tope = nuevo;
+        return true;
     }
 
     @Override
     public boolean desapilar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean resultado = false;
+        //Asigna el tope al enlace del tope actual. Si el enlace es nulo, entonces la Pila quedará vacía.
+        if(this.tope != null)
+        {
+            this.tope = this.tope.getEnlace();
+            resultado = true;
+        }
+        return resultado;
     }
 
     @Override
     public Object obtenerTope() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Object elemento = null;
+        if(this.tope != null)
+        {
+            elemento = this.tope.getElem();
+        }
+        return elemento;
     }
 
     @Override
     public boolean esVacia() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean resultado = false;
+        if(this.tope == null)
+        {
+            resultado = true;
+        }
+        return resultado;
     }
 
     @Override
     public void vaciar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        this.tope = null;
     }
 
     @Override
     public Pila clone() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //Creo nueva Pila
+        Pila pilaClonada = new Pila();
+        //Verifico primero si la pila está vacía. Si no es así, recorro todos los elementos a clonar
+        if(this.tope != null)
+        {
+            clonarPilaRecursivo(pilaClonada, this.tope);
+        }
+        return pilaClonada;
+    }
+    
+    private void clonarPilaRecursivo(Pila pila, Nodo nodo){
+        //Método privado auxiliar del método público Clone
+        //Mientras el nodo sea distinto a nulo, se realiza el paso recursivo para recorrer todos los elementos de la Pila
+        if(nodo != null)
+        {
+            //Paso recursivo
+            clonarPilaRecursivo(pila, nodo.getEnlace());
+            //Paso de clonación de Pila
+            Nodo nuevoNodo = new Nodo(nodo.getElem(), nodo.getEnlace());
+            pila.apilar(nuevoNodo);
+        }
     }
     
     @Override
     public String toString(){
-        return "";
+        String resultado = "";
+        if(this.tope == null)
+        {
+            resultado = "Pila vacía";
+        }else{
+            //Se crea un nodo auxiliar para recorrer la Pila
+            Nodo aux = this.tope;
+            resultado = "[";
+            //Se recorre la pila
+            while(aux != null)
+            {
+                resultado += aux.getElem().toString();
+                aux = aux.getEnlace();
+                if(aux != null)
+                {
+                    resultado +=",";
+                }
+            }
+            resultado += "]";
+        }
+        return resultado;
     }
     
 }
