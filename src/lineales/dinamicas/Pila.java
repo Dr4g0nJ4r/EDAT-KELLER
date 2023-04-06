322ea5fdb0665845e0e3d35f80de0ef1f49149e1
Luis Keller <luis.keller@est.fi.uncoma.edu.ar>
2023-04-06 17:31:29 -0300

    Commit inicial
    Inicio del repositorio EDAT 2023 Luis Keller

---
+++ src/lineales/dinamicas/Pila.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lineales.dinamicas;
+
+import lineales.interfaces.PilaInterface;
+
+
+/**
+ *
+ * @author Luis
+ */
+public class Pila implements PilaInterface{
+    
+}
