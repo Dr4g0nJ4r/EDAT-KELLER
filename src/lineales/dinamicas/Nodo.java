002a91a0560f189316fbb06a37f557724903c881
Luis Keller <luis.keller@est.fi.uncoma.edu.ar>
2023-04-06 17:51:15 -0300

    Actualización Clase Nodo
    Creación de Constructores y métodos publicos

--- src/lineales/dinamicas/Nodo.java
+++ src/lineales/dinamicas/Nodo.java
@@ -4,10 +4,32 @@
  */
 package lineales.dinamicas;
 
-/**
- *
- * @author Luis
- */
+/************* Autores ***********
+KELLER LUIS EULOGIO, Legajo FAI-2321
+*/
 public class Nodo {
+    private Object elem;
+    private Nodo enlace;
     
+    //Constructor completo de Nodo
+    public Nodo(Object elem, Nodo enlace){
+        this.elem = elem;
+        this.enlace = enlace;
+    }
+    
+    public Object getElem(){
+        return this.elem;
+    }
+    
+    public void setElem(Object elem){
+        this.elem = elem;
+    }
+    
+    public Nodo getEnlace(){
+        return this.enlace;
+    }
+    
+    public void setEnlace(Nodo enlace){
+        this.enlace = enlace;
+    }
 }
