5d46ff1b8d7b6e9a5a1ba4f1cf5e09ec6efc7d5b
Dr4g0nJ4r <luis.keller@est.fi.uncoma.edu.ar>
2023-06-04 22:51:08 -0300

    Interface ArbolGenerico Ok

--- src/interfaces/ArbolGenerico_Interface.java
+++ src/interfaces/ArbolGenerico_Interface.java
@@ -10,4 +10,31 @@ package interfaces;
  */
 public interface ArbolGenerico_Interface {
     
+    public boolean insertar(Object elem, Object elemPadre);
+    
+    public boolean pertenece(Object elem);
+    
+    public Lista_Interface ancestros(Object elem);
+    
+    public boolean esVacio();
+    
+    public int altura();
+    
+    public int nivel(Object elem);
+    
+    public Object padre(Object elem);
+    
+    public Lista_Interface listarPreorden();
+    
+    public Lista_Interface listarInorden();
+    
+    public Lista_Interface listarPosorden();
+    
+    public Lista_Interface listarPorNiveles();
+    
+    public ArbolGenerico_Interface clone();
+    
+    public void vaciar();
+    
+    public String toString();
 }
---
+++ src/jerarquicas/dinamicas/ArbolGenerico.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jerarquicas.dinamicas;
+import interfaces.ArbolGenerico_Interface;
+/**
+ *
+ * @author Luis
+ */
+public class ArbolGenerico implements ArbolGenerico_Interface{
+    
+}
